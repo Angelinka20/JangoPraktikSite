79527ba15bd83a047cbc7488c60ed3f6e80b1870
TraIzoR1 <44964223+TraIzoR1@users.noreply.github.com>
2020-12-01 12:23:03 +0200

    Initial commit

---
+++ praktuka/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\yniver\programirovanie\piton\jangopraktiksite\praktuka\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
