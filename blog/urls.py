0ef8a1613d4459084c064a38e66f4dc5a66dee2f
Oleh K <deuskarion@gmail.com>
2020-12-05 17:26:24 +0200

    rozklad

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^rozklad$', views.google_rozklad, name='google_rozklad'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def post_list(request):
-    n = "OLEG"
-    return render(request, 'blog/index.html', context={'name':n})
+    return render(request, 'blog/index.html', {})
+
+def google_rozklad(request):
+    return render(request, 'blog/rozklad.html', {})
