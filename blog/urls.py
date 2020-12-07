14ab8990793c2fb2944b1520445ab925ad629132
TraIzoR1 <44964223+TraIzoR1@users.noreply.github.com>
2020-12-07 15:03:07 +0200

    minor

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^rozklad$', views.google_rozklad, name='google_rozklad'),
-    url(r'^predmets$', views.predmets, name='predmets'),
-    url(r'^news$', views.news, name='news'),
+    url(r'^rozklad/$', views.google_rozklad, name='google_rozklad'),
+    url(r'^predmets/$', views.predmets, name='predmets'),
+    url(r'^news/$', views.news, name='news'),
     url(r'', views.home, name='home'),
-    url(r'^marks$', views.marks, name='marks')
+    url(r'^predmets/marks/$', views.marks, name='marks')
 ]
