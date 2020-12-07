88f9731e5287507226b9f4ae36f2c41793df98ea
Oleh K <deuskarion@gmail.com>
2020-12-07 13:31:42 +0200

    add img

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.ImageField(upload_to='blog/static/images/downloaded', null=True, blank=True, verbose_name='Ссылка картинки')
+    image = models.ImageField(upload_to='blog/static/images/downloaded', null=True, blank=True)
 
     def __str__(self):
         return self.title
