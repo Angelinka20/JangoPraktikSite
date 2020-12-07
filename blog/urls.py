f657c922e9d0f28b1de71ac487d8a1a81fd75503
TraIzoR1 <44964223+TraIzoR1@users.noreply.github.com>
2020-12-07 11:49:21 +0200

    Change models.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^rozklad$', views.google_rozklad, name='google_rozklad'),
+    url(r'^predmets', views.predmets, name='predmets'),
+    url(r'^news', views.news, name='news'),
+    url(r'', views.home, name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
     return render(request, 'blog/index.html', {})
 
 def google_rozklad(request):
     return render(request, 'blog/rozklad.html', {})
+
+def predmets(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'blog/prdmets.html', {'posts': posts})
+
+def news(request):
+    return render(request, 'blog/news.html', {})
+
+def home(request):
+    return render(request, 'blog/base.html', {})
